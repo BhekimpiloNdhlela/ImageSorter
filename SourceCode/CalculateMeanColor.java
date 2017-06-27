7a3284b4058e592a3e048f8c59571c993082d77d
BhekimpiloNdhela <18998712@sun.ac.za>
2017-06-27 16:28:09 +0200

    fixed indent mistakes

--- SourceCode/CalculateMeanColor.java
+++ SourceCode/CalculateMeanColor.java
@@ -17,7 +17,7 @@ public class CalculateMeanColor {
   private Double []meanColorBlue;
 
   /**
-   * This is a construct for the object that calculates the mean of all the components of Red Green and Blue.
+   * This is a construct for the object that calculates the mean of all the components of Red Green and Blue
    * This Constructor receives an array of Strings which holds the name of the image(This is basically the
    * original image list which is read from the image path or directory) and a string which represents
    * the image directory or the image path.
@@ -34,7 +34,7 @@ public class CalculateMeanColor {
   }
 
   /**
-   * This is the method that returns the number of all the images with-in a directory or path. It can also
+   * This is the method that returns the number of all the images with-in a directory or path It can also
    * be read as the length of the image list in the image directory.
    * @return numberOfImages
    */
@@ -43,9 +43,9 @@ public class CalculateMeanColor {
   }
 
   /**
-   * This is the method that gets an array of String which has all the images within a directory. This is
+   * This is the method that gets an array of String which has all the images within a directory This is
    * a list of all the images that are supposed to be processed and sorted according to their Red, Green and
-   * Blue Mean Color Components. This is the original duplicate of the the image list that has not been
+   * Blue Mean Color Components This is the original duplicate of the the image list that has not been
    * sorted yet.
    * @return ImageList
    */
@@ -55,9 +55,9 @@ public class CalculateMeanColor {
 
   /**
    * This Method is a getter of all the Red Mean Components of all the pictures in the image directory or all
-   * the images in the imageList. However, this list of all the image's Red Components is not in sorted order.
-   * It preserves the order of the images in the image list directory. Meaning that every element in this array
-   * corresponds to the index of the image that is of the imageList. for Example, imageList[2] has a Red mean
+   * the images in the imageList, However, this list of all the image's Red Components is not in sorted order,
+   * It preserves the order of the images in the image list directory, Meaning that every element in this array
+   * corresponds to the index of the image that is of the imageList, for Example, imageList[2] has a Red mean
    * component of: meanColorRed[2];
    * @return imageRedMeanComponent
    */
@@ -67,9 +67,9 @@ public class CalculateMeanColor {
 
   /**
    * This Method is a getter of all the Green Mean Components of all the pictures in the image directory or all
-   * the images in the imageList. However, this lisit of all the image's Green Components is not in sorted order.
-   * It preserves the order of the images in the image list directory. Meaning that every element in this array
-   * corresponds to the index of the image that is of the imageList. for Example, imageList[2] has a Green mean
+   * the images in the imageList, However, this lisit of all the image's Green Components is not in sorted order,
+   * It preserves the order of the images in the image list directory, Meaning that every element in this array
+   * corresponds to the index of the image that is of the imageList, for Example, imageList[2] has a Green mean
    * component of: meanColorGreen[2];
    * @return imageGreenMeanComponent
    */
@@ -79,25 +79,25 @@ public class CalculateMeanColor {
 
   /**
    * This Method is a getter of all the Blue Mean Components of all the pictures in the image directory or all
-   * the images in the imageList. However, this list of all the image's Blue Components is not in sorted order.
-   * It preserves the order of the images in the image list directory. Meaning that every element in this array
-   * corresponds to the index of the image that is of the imageList. for Example, imageList[2] has a Blue mean
+   * the images in the imageList, However, this list of all the image's Blue Components is not in sorted order,
+   * It preserves the order of the images in the image list directory, Meaning that every element in this array
+   * corresponds to the index of the image that is of the imageList, for Example, imageList[2] has a Blue mean
    * component of: meanColorBlue[2];
    * @return imageBlueMeanComponent
    */
-  public Double[] getImageBlueMeanComponent(){
+  public Double[] getImageBlueMeanComponent() {
     return meanColorBlue;
   }
 
   /**
    * This is a helper Method that helps calculate the mean of the Red Green and the Blue components of the given
    * image This method calculates the mean of one image which is in the form of a buffered image and returns a
-   * double array which is of length of 3. This array includes the Red mean of the image in index[0], Green mean
+   * double array which is of length of 3 This array includes the Red mean of the image in index[0], Green mean
    * of the image in index[1] and Blue mean of the image in index[2]
    * @param   bufferedImageAtHand
    * @return  imageColorComponents
    */
-  private double[] imageColorMeanValues(BufferedImage bufferedImage){
+  private double[] imageColorMeanValues(BufferedImage bufferedImage) {
     double redSigma   = 0.0;
     double greenSigma = 0.0;
     double blueSigma  = 0.0;
@@ -105,8 +105,8 @@ public class CalculateMeanColor {
     int heightOfImage = bufferedImage.getHeight();
     int widthOfImage  = bufferedImage.getWidth();
     int pixel;
-    for(int i = 0; i < heightOfImage; i++){
-      for(int j = 0; j < widthOfImage; j++){
+    for(int i = 0; i < heightOfImage; i++) {
+      for(int j = 0; j < widthOfImage; j++) { 
         pixel = bufferedImage.getRGB(j, i);
         redSigma     += (pixel >> 16) & 0xff;
         greenSigma   += (pixel >> 8) & 0xff;
@@ -117,13 +117,13 @@ public class CalculateMeanColor {
   }
 
   /**
-   * This is the Method that Calculates the mean of all the color components. For example it calculates the Mean
+   * This is the Method that Calculates the mean of all the color components, For example it calculates the Mean
    * of Red, Green and Blue by the aid of a helper method imageColorMeanValues.
    * @param  imageDirectory
    * @throws IOException
    */
   public void imageColorMeanValues() throws IOException{
-    for(int i = 0; i < numberOfImages; i++ ){
+    for(int i = 0; i < numberOfImages; i++ ) {
       double []colorMean = imageColorMeanValues(ImageIO.read(new File(imageDirectory.concat(imageList[i]))));
       meanColorRed[i]    = colorMean[0];
       meanColorGreen[i]  = colorMean[1];
